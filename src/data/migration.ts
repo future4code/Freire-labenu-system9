98930f1b2477af33914e015b3c8bf2fb756ce9f5
Leonardo-almd <104574298+Leonardo-almd@users.noreply.github.com>
2022-09-08 11:15:19 -0300

    introducao

---
+++ src/data/migration.ts
@@ -0,0 +1,42 @@
+import { connection } from "./connection"
+const printError = (error: any) => { console.log(error.sqlMessage || error.message) }
+
+const createTableEstudantes = () => connection.raw(`
+      CREATE TABLE IF NOT EXISTS estudante (
+         id VARCHAR(255) PRIMARY KEY,
+         nome VARCHAR(255) NOT NULL,
+         email VARCHAR(255) NOT NULL UNIQUE,
+         data_nasc DATE NOT NULL,
+         turma_id VARCHAR(255) FOREING KEY NOT NULL         
+      );
+   `)
+    .then(() => { console.log("Tabela criada") })
+    .catch(printError)
+
+const createTableTurma = () => connection.raw(`
+   CREATE TABLE IF NOT EXISTS turma (
+      id VARCHAR(255) PRIMARY KEY,
+      nome VARCHAR(255),
+      modulo VARCHAR(255) DEFAULT 0        
+   );
+`)
+    .then(() => { console.log("Tabela criada") })
+    .catch(printError)
+
+    const createTableDocente = () => connection.raw(`
+    CREATE TABLE IF NOT EXISTS docente (
+       id VARCHAR(255) PRIMARY KEY,
+       nome VARCHAR(255),
+       email VARCHAR(255) NOT NULL UNIQUE,       
+       data_nasc DATE NOT NULL,
+       turma_id VARCHAR(255) FOREING KEY NOT NULL            
+    );
+ `)
+     .then(() => { console.log("Tabela criada") })
+     .catch(printError)    
+
+const closeConnection = () => { connection.destroy() }
+
+createTableEstudantes()
+    .then(() => console.log("Banco pronto!"))
+    .finally(closeConnection)
---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export type Turma = {
+    id: string,
+    nome: string,
+    docentes: string[],
+    estudantes: string[],
+    modulo: string
+}
+
+export type Estudante = {
+    id: string, 
+    nome: string,
+    email: string,
+    data_nasc: string,
+    turma_id: string
+}
+
+export type Docente = {
+    id: string,
+    nome: string,
+    email: string,
+    data_nasc: string,
+    turma_id: string
+}
